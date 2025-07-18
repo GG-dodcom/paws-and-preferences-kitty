d4ad12a1f21754a54b66294b944a60c7ed99a91d
GG-dodcom <68137675+GG-dodcom@users.noreply.github.com>
2025-07-18 18:56:57 +0800

    deploy

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,6 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   basePath: '/paws-and-preferences-kitty', // Matches GitHub Pages repository name
-  assetPrefix: '/paws-and-preferences-kitty/',
 };
 
 export default nextConfig;
